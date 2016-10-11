3e03c88f39add6089286a561889f9550963a8b81
Julian Barnett <jbarnett@tableau.com>
2016-10-11 16:25:43 -0700

    adding distro logic into serverspec.sh

--- cloud/linux/scripts/serverspec.sh
+++ cloud/linux/scripts/serverspec.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-sudo yum install -y ruby ruby-devel
+DISTRO=$(lsb_release -i | awk '{print $3}')
+
+if [ "$DISTRO" = "CentOS" ]; then
+   sudo yum install -y ruby ruby-devel
+fi
+
+if [ "$DISTRO" = "Ubuntu" ]; then
+   sudo apt-add-repository -y ppa:brightbox/ruby-ng
+   sudo apt-get update
+   sudo apt-get install -y ruby2.3
+   # Not used yet
+fi
+
 sudo gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 sudo /usr/local/bin/bundle install --path=vendor
