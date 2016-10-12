5b80f019aa2160032085608dea76f0bd3182bd32
Julian Barnett <jbarnett@tableau.com>
2016-10-12 10:35:58 -0700

    adding better logic to cron script for centos7/ubuntu1404

--- cloud/linux/packer_cron.sh
+++ cloud/linux/packer_cron.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+MINPARAMS=1
+
+if [ $# -lt "$MINPARAMS" ]
+then
+  echo
+  echo "This script needs at least $MINPARAMS command-line arguments!"
+  echo "Ex: bash \"`basename $0`\" centos7|ubuntu1404"
+  exit 1
+fi
+
+# Change to working dir
 cd /usr/local/devit/packer/cloud/linux/
 . openrc
-
-# Generate quarter and year string and full image name
-QDATE=$(date +%Y)Q$(( ($(date +%-m)-1)/3+1 ))
-IMAGE_NAME="CentOS7-DevIT-Final-$QDATE"
 
 # Create packer log env vars
 PACKER_LOG=1
 PACKER_LOG_PATH=centos7-x64-$QDATE.log
 
-# Replace packer template with current quarter and year image name template
-sed "s/IMAGE_NAME/$IMAGE_NAME/g" centos7-x64-cloud.json > centos7-x64-cloud-$QDATE.json
+# Generate quarter and year string and full image name
+QDATE=$(date +%Y)q$(( ($(date +%-m)-1)/3+1 ))
+IMAGE_NAME="$1-devit-final-$QDATE"
 
-/usr/local/bin/packer build centos7-x64-cloud-$QDATE.json
+# Replace packer template with current quarter and year image name template
+sed "s/IMAGE_NAME/$IMAGE_NAME/g" "$1"-x64-cloud.json > "$1"-x64-cloud-$QDATE.json
+
+/usr/local/bin/packer build "$1"-x64-cloud-$QDATE.json
