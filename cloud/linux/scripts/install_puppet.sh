09fde20b7f9c44ce2a2acf973c6ccf1c90cb571f
Julian Barnett <jbarnett@tableau.com>
2016-11-02 16:19:22 -0700

    creating wrapper scripts for domain join and install puppet to pull from git instead of embed directly in image

--- cloud/linux/scripts/install_puppet.sh
+++ cloud/linux/scripts/install_puppet.sh
@@ -6,45 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-swver=$(lsb_release -d | awk '{print $2}')
-
-if [ $swver = "CentOS" ]; then
-
-   # CentOS release version
-   releasever=$(lsb_release -r | awk '{print $2}' | cut -c 1)
-
-   # Install puppet repo
-   rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-$releasever.noarch.rpm
-
-   # Install latest puppet agent
-   yum install -y puppet
-
-   # Add in Tableau specific puppet.conf
-   curl -o /etc/puppet/puppet.conf http://puppetshare.dev.tsi.lan/puppet/Linux/PuppetOG/puppet.conf
-
-   # Enable and start puppet service
-   systemctl enable puppet.service
-   systemctl start puppet.service
-fi
-
-if [ $swver = "Ubuntu" ]; then
-
-   # Ubuntu release
-   releasename=$(lsb_release -c | awk '{print $2}')
-
-   # Install apt repo
-   cd /tmp
-   wget https://apt.puppetlabs.com/puppetlabs-release-$releasename.deb
-   sudo dpkg -i /tmp/puppetlabs-release-$releasename.deb
-   sudo apt-get update
-
-   # Install latest puppet agent
-   sudo apt-get install -y puppet
-
-   # Add in Tableau specific puppet.conf
-   curl -o /etc/puppet/puppet.conf http://puppetshare.dev.tsi.lan/puppet/Linux/PuppetOG/puppet.conf
-
-   # Enable and start puppet service
-   sudo sed -i 's/^START.*/START=yes/' /etc/default/puppet
-   sudo service puppet start
-fi
+cd /tmp
+git clone https://gitlab.tableausoftware.com/devit/puppet_agent.git
+bash /tmp/puppet_agent/install_puppet.sh
+rm -rf /tmp/puppet_agent
