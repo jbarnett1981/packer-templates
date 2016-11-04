01e384877f0f8a9c5a9d1764366d6caac1c1f17e
Julian Barnett <jbarnett@tableau.com>
2016-11-04 11:37:56 -0700

    lvm extend sudo

--- cloud/linux/scripts/lvm_extend.sh
+++ cloud/linux/scripts/lvm_extend.sh
@@ -1,4 +1,4 @@
-cat > /usr/local/share/EXPAND_ROOT <<EOF
+sudo cat > /usr/local/share/EXPAND_ROOT <<EOF
 disk="sda"
 startsector=\$(fdisk -u -l /dev/\$disk | grep \${disk}2 | awk '{print \$2}')
 parted /dev/\$disk --script rm 2
@@ -7,4 +7,4 @@ parted /dev/\$disk --script set 2 lvm on
 pvresize /dev/\${disk}2
 lvextend --extents +100%FREE /dev/mapper/vg00-lv_root --resizefs
 EOF
-chmod +x /usr/local/share/EXPAND_ROOT
+sudo chmod +x /usr/local/share/EXPAND_ROOT
