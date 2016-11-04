abd4fd0697ab466b448d27e912265f5967379ac1
Julian Barnett <jbarnett@tableau.com>
2016-11-04 12:06:36 -0700

    this resizing process is tough

--- cloud/linux/scripts/lvm_extend.sh
+++ cloud/linux/scripts/lvm_extend.sh
@@ -1,10 +1,8 @@
-sudo cat > /usr/local/share/EXPAND_ROOT <<EOF
+#!/bin/bash
 disk="sda"
-startsector=\$(fdisk -u -l /dev/\$disk | grep \${disk}2 | awk '{print \$2}')
-parted /dev/\$disk --script rm 2
-parted /dev/\$disk --script "mkpart primary ext4 \${startsector}s -1s"
-parted /dev/\$disk --script set 2 lvm on
-pvresize /dev/\${disk}2
+startsector=$(fdisk -u -l /dev/$disk | grep ${disk}2 | awk '{print $2}')
+parted /dev/$disk --script rm 2
+parted /dev/$disk --script "mkpart primary ext4 ${startsector}s -1s"
+parted /dev/$disk --script set 2 lvm on
+pvresize /dev/${disk}2
 lvextend --extents +100%FREE /dev/mapper/vg00-lv_root --resizefs
-EOF
-sudo chmod +x /usr/local/share/EXPAND_ROOT
