9ec7a80104ba20fbdb45bbde33021228caa42f66
Julian Barnett <jbarnett@tableau.com>
2016-11-16 11:06:27 -0800

    removed tableau-devit sudoers file. puppet will do this. fixing spec file

--- cloud/linux/tests/spec/sudoers_spec.rb
+++ cloud/linux/tests/spec/sudoers_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
-describe file('/etc/sudoers.d/tableau-devit') do
-  its(:md5sum) { should eq '7b256f53fe2bf46e5d1f60d1e641450c' }
-end
-
-describe file('/etc/sudoers.d/tableau-devit') do
-  it { should be_mode 644 }
+describe file('/etc/sudoers') do
+  its(:content) { should match /Defaults    !requiretty/ }
 end
