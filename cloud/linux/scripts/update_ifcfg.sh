d96ecf3e7ee55efb29bc582b4a817f79d513fa1c
Julian Barnett <jbarnett@tableau.com>
2016-12-08 15:44:18 -0800

    fixes

--- cloud/linux/scripts/update_ifcfg.sh
+++ cloud/linux/scripts/update_ifcfg.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Remove NetworkManager, it sucks
+export IFCFG=/etc/sysconfig/network-scripts/ifcfg-eth0
+export HWADDR=$(ip addr show dev eth0 | grep ether | awk '{print $2}')
+
 systemctl stop NetworkManager
 chkconfig NetworkManager off
 yum erase -y NetworkManager
 chkconfig network on
 
-export IFCFG=/etc/sysconfig/network-scripts/ifcfg-eth0
 # Add/change PEERDNS=no in ifcfg-eth0
 if grep -q PEERDNS "$IFCFG"; then sudo -E sed -i 's/^PEERDNS.*/PEERDNS=no/' $IFCFG; else sudo -E bash -c 'echo "PEERDNS=no" >> $IFCFG'; fi
 
 # Add correct HWADDR to ifcfg-eth0
-export HWADDR=$(ip addr show dev eth0 | grep ether | awk '{print $2}')
 if grep -q HWADDR "$IFCFG"; then sudo -E sed -i "s/^HWADDR.*/HWADDR=${HWADDR}/" $IFCFG; else sudo -E bash -c 'echo "HWADDR=${HWADDR}" >> $IFCFG'; fi
 
 # And finally start the network service
