4639de4ddc9ae39a4f4b2cd443c2b582b4dbae9d
Julian Barnett <jbarnett@tableau.com>
2016-12-08 17:11:01 -0800

    fixes

--- cloud/linux/scripts/update_ifcfg.sh
+++ cloud/linux/scripts/update_ifcfg.sh
@@ -10,14 +10,13 @@ yum erase -y NetworkManager
 chkconfig network on
 
 # Add/change PEERDNS=no in ifcfg-eth0
-if grep -q PEERDNS "$IFCFG"; then sudo -E sed -i 's/^PEERDNS.*/PEERDNS=no/' $IFCFG; else sudo -E bash -c 'echo "PEERDNS=no" >> $IFCFG'; fi
+if grep -q PEERDNS "$IFCFG"; then sed -i 's/^PEERDNS.*/PEERDNS=no/' $IFCFG; else bash -c 'echo "PEERDNS=no" >> $IFCFG'; fi
 
 # Add correct HWADDR to ifcfg-eth0
-if grep -q HWADDR "$IFCFG"; then sudo -E sed -i "s/^HWADDR.*/HWADDR=${HWADDR}/" $IFCFG; else sudo -E bash -c 'echo "HWADDR=${HWADDR}" >> $IFCFG'; fi
+if grep -q HWADDR "$IFCFG"; then sed -i "s/^HWADDR.*/HWADDR=${HWADDR}/" $IFCFG; else bash -c 'echo "HWADDR=${HWADDR}" >> $IFCFG'; fi
 
 # And finally start the network service
 systemctl start network
 
 # Cleanup
 rm -f /etc/cron.d/update_ifcfg
-/sbin/reboot
