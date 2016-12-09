048ea66d72bd0d7a6d6bf93a2dd34d65df92e4d5
Julian Barnett <jbarnett@tableau.com>
2016-12-08 17:56:50 -0800

    fixes

--- cloud/linux/scripts/update_ifcfg.sh
+++ cloud/linux/scripts/update_ifcfg.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 # Remove NetworkManager, it sucks
-export IFCFG=/etc/sysconfig/network-scripts/ifcfg-eth0
-export HWADDR=$(ip addr show dev eth0 | grep ether | awk '{print $2}')
-
+IFCFG=/etc/sysconfig/network-scripts/ifcfg-eth0
+HWADDR=$(ip addr show dev eth0 | grep ether | awk '{print $2}')
+echo $HWADDR
 systemctl stop NetworkManager
 chkconfig NetworkManager off
 yum erase -y NetworkManager
 chkconfig network on
 
 # Add/change PEERDNS=no in ifcfg-eth0
-if grep -q PEERDNS "$IFCFG"; then sed -i 's/^PEERDNS.*/PEERDNS=no/' $IFCFG; else bash -c 'echo "PEERDNS=no" >> $IFCFG'; fi
+if grep -q PEERDNS "$IFCFG"; then sed -i "s/^PEERDNS.*/PEERDNS=no/g" $IFCFG; else echo "PEERDNS=no" >> $IFCFG; fi
 
 # Add correct HWADDR to ifcfg-eth0
-if grep -q HWADDR "$IFCFG"; then sed -i "s/^HWADDR.*/HWADDR=${HWADDR}/" $IFCFG; else bash -c 'echo "HWADDR=${HWADDR}" >> $IFCFG'; fi
+if grep -q HWADDR "$IFCFG"; then sed -i "s/^HWADDR.*/HWADDR=$HWADDR/g" $IFCFG; else echo "HWADDR=$HWADDR" >> $IFCFG; fi
 
 # And finally start the network service
 systemctl start network
